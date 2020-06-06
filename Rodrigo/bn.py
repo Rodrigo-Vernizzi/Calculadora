baae632be954ae6146e5e308affcc0460a0e7872
Rodrigo caramaschi <vernizzirodrigo@gmail.com>
2020-06-06 12:00:37 -0300

    nao sei oq to errando

---
+++ Rodrigo/bn.py
@@ -0,0 +1,28 @@
+import math
+
+
+
+print('F(x) = ax^2 + bx + c')
+
+a = int(input('Digite o valor de a: '))
+b = int(input('Digite o valor de b: '))
+c = int(input('Digite o valor de c: '))
+
+
+if a == 0:
+    x = -c/b
+    print(f'A raiz é {x}')
+
+
+if  b**2 -4 * a * c << 0:
+    print('Não existe raizes reais')
+
+elif a != 0:
+
+    x1_sg = (-b + math.sqrt(b**2 -4 * a * c)) / (2*a)
+    x2_sg = (-b - math.sqrt(b**2 -4 * a * c)) / (2 * a)
+
+    print(f'As raizes são {x1_sg}, {x2_sg}')
+
+
+
